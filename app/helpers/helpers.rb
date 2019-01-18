75589b53ea40ed59639a5afaa213aff71f3db5a1
Virginia Dooley <vcdooley@gmail.com>
2019-01-18 11:35:45 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+
+#class method to find the user_id of the current session
+  def self.current_user(session)
+    User.find(session[:user_id])
+  end
+
+  # The double negation forces the return value to be a boolean.
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+  end
+
 end
